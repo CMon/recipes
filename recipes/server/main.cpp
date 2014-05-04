fb395e87c861818601bbe32a5704f6382d923643
Simon Schäfer <simon.schaefer@koeln.de>
2014-05-04 23:52:39 +0200

    added cflib server stuff to recipe-server

--- recipes/server/main.cpp
+++ recipes/server/main.cpp
@@ -1,9 +1,35 @@
 #include <QCoreApplication>
 
+#include <services/userservice.h>
+
+#include <cflib/util/log.h>
+#include <cflib/db/db.h>
+#include <cflib/http/apiserver.h>
+#include <cflib/http/server.h>
+
+USE_LOG(LogCat::Etc)
+
 int main(int argc, char ** argv)
 {
     QCoreApplication a(argc, argv);
     a.setApplicationName("Receipts Server");
 
-    return a.exec();
+    cflib::util::Log::start("recipes-server.log");
+    cflib::db::setParameter("recipes", "recipesRoot", "sql");
+
+    UserService userService;
+
+    cflib::http::ApiServer api;
+    api.registerService(&userService);
+
+    cflib::http::Server serv;
+    serv.registerHandler(&api);
+    if (!serv.start(8080)) {
+        QTextStream(stderr) << "cannot start HTTP-Server (port already in use)" << endl;
+        return 1;
+    }
+
+    int retval = a.exec();
+    logInfo("terminating softly with retval: %1", retval);
+    return retval;
 }
--- recipes/server/server.pro
+++ recipes/server/server.pro
@@ -1,15 +1,20 @@
 !include(../config.pri):error("Could not find config.pri")
 
-QT       += core network
+QT       += network sql
 QT       -= gui
 
 TARGET = server
 
 CONFIG   += console
 CONFIG   -= app_bundle
+
 TEMPLATE = app
 
 SOURCES += \
     main.cpp
 
+useLibs(cflib_db cflib_http cflib_crypt cflib_serialize cflib_util)
+
 useLibs($$RECIPES_ROOT/lib, common_lib database_lib services_lib)
+
+app()
