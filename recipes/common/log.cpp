8e85ad4a5a7166de8e7197180a26463b1109fd9d
Simon Schäfer <simon.schaefer@koeln.de>
2019-04-09 23:09:49 +0200

    add color output to logger

--- recipes/common/log.cpp
+++ recipes/common/log.cpp
@@ -17,25 +17,36 @@ bool RecipeLog::initFile(const QString & filename)
 	return true;
 }
 
-static void logMessage(FILE * stream, QtMsgType type, const QMessageLogContext &context, const QString &msg)
+static const char * colorReset  = "\033[0m";
+static const char * colorGreen  = "\033[0;32m";
+static const char * colorYellow = "\033[0;33m";
+static const char * colorRed    = "\033[0;31m";
+static const char * colorBlink  = "\033[0;5m";
+
+static void logMessage(FILE * stream, QtMsgType type, const QMessageLogContext &context, const QString &msg, bool withColor)
 {
+	const char * colorPrefix = colorReset;
+
 	const QDateTime now = QDateTime::currentDateTimeUtc();
 	char typeStr;
 	switch (type) {
 		case QtDebugMsg:    typeStr = 'D'; break;
-		case QtInfoMsg:     typeStr = 'I'; break;
-		case QtWarningMsg:  typeStr = 'W'; break;
-		case QtCriticalMsg: typeStr = 'C'; break;
-		case QtFatalMsg:    typeStr = 'F'; break;
+		case QtInfoMsg:     typeStr = 'I'; colorPrefix = colorGreen;  break;
+		case QtWarningMsg:  typeStr = 'W'; colorPrefix = colorYellow; break;
+		case QtCriticalMsg: typeStr = 'C'; colorPrefix = colorRed;    break;
+		case QtFatalMsg:    typeStr = 'F'; colorPrefix = colorBlink;  break;
 	}
-	fprintf(stream, "%s %c %20s:%4d %15s:  %s\n", qPrintable(now.toString(Qt::ISODate)), typeStr, basename(const_cast<char*>(context.file)), context.line, context.category, msg.toLocal8Bit().constData());
+	if (withColor) {
+		fprintf(stream, "%s%s %c %20s:%4d %15s:  %s%s\n", colorPrefix, qPrintable(now.toString(Qt::ISODate)), typeStr, basename(const_cast<char*>(context.file)), context.line, context.category, msg.toLocal8Bit().constData(), colorReset);
+	} else {
+		fprintf(stream, "%s %c %20s:%4d %15s:  %s\n", qPrintable(now.toString(Qt::ISODate)), typeStr, basename(const_cast<char*>(context.file)), context.line, context.category, msg.toLocal8Bit().constData());
+	}
 	fflush(stream);
-
 }
 
 void RecipeLog::consoleMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
-	logMessage(stdout, type, context, msg);
+	logMessage(stdout, type, context, msg, true);
 }
 
 void RecipeLog::fileMessageHandler(QtMsgType type, const QMessageLogContext & context, const QString & msg)
@@ -45,5 +56,5 @@ void RecipeLog::fileMessageHandler(QtMsgType type, const QMessageLogContext & co
 		return;
 	}
 
-	logMessage(logfile, type, context, msg);
+	logMessage(logfile, type, context, msg, false);
 }
